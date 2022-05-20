49596291e5f429e453fa0861b48bc7903c782714
Victor Murilo <victor2706.vm@gmail.com>
2022-05-20 15:25:52 -0300

    Cumpri o requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  }
+  if (ids.length === 1) {
+    return [data.species.find((element) => element.id === ids[0])];
+  }
+  const especies = [];
+  ids.forEach((idAtual) => especies.push(data.species.find((element) => element.id === idAtual)));
+  return especies;
 }
-
+// console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
+// find((element) => element.id === ids[0])
 module.exports = getSpeciesByIds;
