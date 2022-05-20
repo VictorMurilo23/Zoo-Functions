9b8524b566ee6e376b3d08e412300d3d3f3bf861
Victor Murilo <victor2706.vm@gmail.com>
2022-05-20 19:11:56 -0300

    Cumpri o requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aquifhfhaufaufagdwuag
+  return entrants.reduce((acc, cur) => {
+    if (cur.age < 18) {
+      acc.child += 1;
+    } else if (cur.age < 50) {
+      acc.adult += 1;
+    }
+    if (cur.age >= 50) {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const child = countEntrants(entrants).child * 20.99;
+  const adult = countEntrants(entrants).adult * 49.99;
+  const senior = countEntrants(entrants).senior * 24.99;
+  return child + adult + senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
