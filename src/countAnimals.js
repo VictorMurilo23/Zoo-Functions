3999b0f9bee23d8b388acf61a4726760763bbe7c
Victor Murilo <victor2706.vm@gmail.com>
2022-05-20 17:39:43 -0300

    Cumpri o requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
-
+// total animais 9
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, cur) => {
+      acc[cur.name] = cur.residents.length;
+      return acc;
+    }, {});
+  }
+
+  const animalSelecionado = data.species.find((element) => element.name === animal.specie);
+  if (animal.sex === undefined) {
+    return animalSelecionado.residents.reduce((acc) => acc + 1, 0);
+  }
+
+  return animalSelecionado.residents.reduce((acc, cur) => {
+    if (cur.sex === animal.sex) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
 }
 
 module.exports = countAnimals;
