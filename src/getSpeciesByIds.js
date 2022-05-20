2e59c32a09406e0ce9b0d88a1c3f1a929f8fbd82
Victor Murilo <victor2706.vm@gmail.com>
2022-05-20 15:39:17 -0300

    Cumpri o requisito 2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -11,6 +11,5 @@ function getSpeciesByIds(...ids) {
   ids.forEach((idAtual) => especies.push(data.species.find((element) => element.id === idAtual)));
   return especies;
 }
-// console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed', 'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
-// find((element) => element.id === ids[0])
+
 module.exports = getSpeciesByIds;
