8e3f2cc6642a015dcc933649a0b0c6185d03e483
Victor Murilo <victor2706.vm@gmail.com>
2022-05-20 16:51:24 -0300

    Cumpri o requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
-
+/* Gerentes:
+stephanieId
+burlId
+olaId
+b0dc644a-5335-489b-8a2c-4e086c7819a2 (Emery Elser)
+*/
 function isManager(id) {
-  // seu código aqui
+  return data.employees.reduce((acc, cur) => {
+    if (cur.managers.some((element) => element === id)) {
+      return true;
+    }
+    return acc;
+  }, false);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.reduce((acc, cur) => {
+      if (cur.managers.some((element) => element === managerId)) {
+        acc.push(`${cur.firstName} ${cur.lastName}`);
+      }
+      return acc;
+    }, []);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
